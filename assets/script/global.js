e68d7cd3a14acbbde210b19433ff279dd4df96c3
samuelds <samuelds.pro@gmail.com>
2020-10-14 19:25:42 +0200

    Update UI

--- assets/script/global.js
+++ assets/script/global.js
@@ -1,8 +1,7 @@
 let defaultStatus = "crewmate";
 
-function resetStatus(card, text) {
-    card.removeClass('isDead');
-    const status = card.find('.status');
+function resetStatus(player, text) {
+    const status = player.find('.status');
     status.removeClass('impostor');
     status.removeClass('suspect');
     status.html(text);
@@ -19,52 +18,58 @@ $(window).on("load", function(){
     
     $("#default-suspect").click(function(event) {
         event.preventDefault();
-        defaultStatus = "suspect";        
+        defaultStatus = "suspect";
         $("#default-suspect").addClass("active");
         $("#default-crewmate").removeClass("active");
     });
     
     $("#removed").click(function(event){
         event.preventDefault();
-        $(".card.removed").removeClass('removed');
+        $(".player.removed").removeClass('removed');
     });
     
     $("#reset").click(function(event){
         event.preventDefault();
-        resetStatus($(".card"), defaultStatus);
+        resetStatus($(".player"), defaultStatus);
+        $(".player").removeClass('isDead');
         if (defaultStatus == "suspect") {
-            $(".card .status").addClass('suspect');
+            $(".player .status").addClass('suspect');
         }        
-        $(".card.removed").removeClass('removed');
-    });
-    
-    $('.crewmate').click(function(event) {
-        const card = $(this).closest('.card');
-        resetStatus(card, "crewmate");
-    });
-
-    $('.suspect').click(function(event) {
-        const card = $(this).closest('.card');
-        resetStatus(card, "suspect");
-        card.find(".status").addClass('suspect');
-    });
-    
-    $('.impostor').click(function(event) {
-        const card = $(this).closest('.card');
-        resetStatus(card, "impostor");
-        card.find(".status").addClass('impostor');
+        $(".player.removed").removeClass('removed');
     });
 
     $('.dead').click(function(event) {
-        const card = $(this).closest('.card');
-        resetStatus(card, "dead");
-        card.addClass('isDead');
+        const player = $(this).closest('.player');
+        player.toggleClass('isDead');
     });
     
     $('.remove').click(function(event) {
-        const card = $(this).closest('.card');
-        resetStatus(card, "removed");
-        card.addClass('removed');
+        const player = $(this).closest('.player');
+        resetStatus(player, "removed");
+        player.addClass('removed');
+    });
+
+    $('.fa-caret-left').click(function(event) {
+        const player = $(this).closest('.player');
+        const status = player.find('.status');
+        if (status.text() == "suspect") {
+            resetStatus(player, "crewmate");
+        } else if (status.text() == "impostor") {      
+            resetStatus(player, "suspect");
+            status.addClass('suspect');
+        }
+    });
+
+    $('.fa-caret-right').click(function(event) {
+        const player = $(this).closest('.player');
+        const status = player.find('.status');
+        if (status.text() == "crewmate") {
+            resetStatus(player, "suspect");
+            status.addClass('suspect');
+        } else if (status.text() == "suspect") {
+            resetStatus(player, "impostor");
+            status.addClass('impostor');
+        }
     });
     
 });
